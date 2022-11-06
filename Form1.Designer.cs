0466b5520e812f1776d3e227ebb57063758492e6
NyiteX <kitheus09@gmail.com>
2022-11-06 17:49:11 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Basic_Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X_Button = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // X_Button
+            // 
+            this.X_Button.AutoSize = true;
+            this.X_Button.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.X_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.X_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X_Button.Location = new System.Drawing.Point(479, 9);
+            this.X_Button.Name = "X_Button";
+            this.X_Button.Size = new System.Drawing.Size(23, 22);
+            this.X_Button.TabIndex = 0;
+            this.X_Button.Text = "X";
+            this.X_Button.Click += new System.EventHandler(this.X_Button_Click);
+            this.X_Button.MouseEnter += new System.EventHandler(this.X_Button_MouseEnter);
+            this.X_Button.MouseLeave += new System.EventHandler(this.X_Button_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(79, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "2";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(181, 46);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(137, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dateTimePicker1_MouseDown);
+            this.dateTimePicker1.MouseEnter += new System.EventHandler(this.dateTimePicker1_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(183, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 108);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "=(";
+            // 
+            // start_1
+            // 
+            this.start_1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.start_1.Location = new System.Drawing.Point(0, 416);
+            this.start_1.Name = "start_1";
+            this.start_1.Size = new System.Drawing.Size(512, 126);
+            this.start_1.TabIndex = 7;
+            this.start_1.Text = "START";
+            this.start_1.UseVisualStyleBackColor = true;
+            this.start_1.Click += new System.EventHandler(this.start_1_Click);
+            this.start_1.MouseEnter += new System.EventHandler(this.start_1_MouseEnter);
+            // 
+            // Basic_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 542);
+            this.Controls.Add(this.start_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.X_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Basic_Window";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Basic_Window_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Basic_Window_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label X_Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start_1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Basic_Window : Form
+    {
+        Point p;
+        int kol_start = 0;
+        public Basic_Window()
+        {
+            InitializeComponent();
+        }
+
+        //Basic_Window
+        private void Basic_Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = new Point(e.X, e.Y);
+        }
+        private void Basic_Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - p.X;
+                Top += e.Y - p.Y;
+            }
+        }
+
+        //Basic_buttons
+        private void X_Button_MouseEnter(object sender, EventArgs e)
+        {
+            X_Button.BorderStyle = BorderStyle.Fixed3D;
+            X_Button.BackColor = Color.Thistle;
+        }
+        private void X_Button_MouseLeave(object sender, EventArgs e)
+        {
+            X_Button.BorderStyle = BorderStyle.FixedSingle;
+            X_Button.BackColor = Color.WhiteSmoke;
+        }
+        private void X_Button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void dateTimePicker1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void start_1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime dayT = new DateTime();
+                dayT = dateTimePicker1.Value;
+                DateTime dayNow = DateTime.Now;
+                label1.Visible = true;
+                if (dayT > dayNow)
+                {
+                    TimeSpan tempDay = dayT - dayNow;
+                    label1.Text = "Секунд прошло: " + tempDay.TotalSeconds;
+                }
+                else
+                    label1.Text = "Эта дата уже прошла!";
+            }
+            catch (Exception E) { MessageBox.Show(E.Message); }
+        }
+
+        private void start_1_MouseEnter(object sender, EventArgs e)
+        {
+            if (kol_start < 2)
+            {
+                start_1.Visible = false;
+                kol_start++;
+            }
+        }
+        private void dateTimePicker1_MouseEnter(object sender, EventArgs e)
+        {
+            start_1.Visible = true;
+        }
+    }
+}
